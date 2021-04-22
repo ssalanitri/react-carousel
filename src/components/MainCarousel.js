a11a4fb0452335c537c3b0c477e0f32acc03b47b
ssalanitri <sergiosalanitri@gmail.com>
2021-04-22 18:30:22 -0300

    more flexible

--- src/components/MainCarousel.js
+++ src/components/MainCarousel.js
@@ -5,20 +5,13 @@ import { Carousel } from 'react-responsive-carousel';
 import { Container, makeStyles } from '@material-ui/core';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-// import img1 from '../assets/img/img1.jpg';
-// import img2 from '../assets/img/img2.jpg';
-// import img3 from '../assets/img/img3.jpg';
-// import img4 from '../assets/img/img4.jpg';
-// import img5 from '../assets/img/img5.jpg';
-// import img6 from '../assets/img/img6.jpg';
-// import img7 from '../assets/img/img7.jpg';
 
 import { Images } from '../data/images';
 
 
 const useStyles = makeStyles({
   img: {
-    width: 600,
+    width: 800,
   },
 });
 
@@ -40,35 +33,6 @@ const MainCarousel = () => {
                 </div>
              ))        
             }
-
-            {/* <Container className={classes.img} >
-                <img alt="" src={img1} className={classes.img}/>
-                <p className="legend">Legend 1</p>
-            </Container>
-            <Container>
-                <img alt="" src={img2} className={classes.img}/>
-                <p className="legend">Legend 2</p>
-            </Container>
-            <Container>
-                <img alt="" src={img3} className={classes.img}/>
-                <p className="legend">Legend 3</p>
-            </Container>
-            <Container>
-                <img alt="" src={img4} className={classes.img}/>
-                <p className="legend">Legend 4</p>
-            </Container>
-            <Container>
-                <img alt="" src={img5} className={classes.img}/>
-                <p className="legend">Legend 5</p>
-            </Container>
-            <Container>
-                <img alt="" src={img6} className={classes.img}/>
-                <p className="legend">Legend 6</p>
-            </Container>
-            <Container>
-                <img alt="" src={img7} className={classes.img}/>
-                <p className="legend">Legend 7</p>
-            </Container> */}
        </Carousel>
 
               
