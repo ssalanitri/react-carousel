3e54560b1e5f32fb253d269af79daea84dbdf735
ssalanitri <sergiosalanitri@gmail.com>
2021-04-22 01:32:36 -0300

    Improvement version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react'
 import MainCarousel from './components/MainCarousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 
-export default function App() {
+import React from 'react'
+import { Container, makeStyles } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+
+
+
+const useStyles = makeStyles({
+  root: {
+    alignContent: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft:100,
+    marginRight:100,
+
+  },
+  container: {
+    height:800,
+    width:800,
+    alignContent: 'center',
+  },
+});
+
+const App = () => {
+
+  const classes = useStyles();
+
   return (
     <>
-        <MainCarousel />
+    <Container className={classes.container}>
+    <Typography variant='h3' component='div' className={classes.root} >React Carousel</Typography>
+      <MainCarousel/>
+    </Container>
     </>
   )
 }
+
+export default App
